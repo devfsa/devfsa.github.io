7a4f53c9f6476441e9067832c51ce6e2da0629ad
Diego Leite <diegojleite@gmail.com>
2019-04-05 16:36:40 -0300

    Integration with YouTube Data API

---
+++ js/main.js
@@ -0,0 +1,96 @@
+fetch('https://api.github.com/repos/devfsa/vagas/issues')
+.then((response) => {
+  response.json().then((data) => {
+    let jobs = data.map(d => {
+      const splitTag = /\s+(?:n[ao]|at)\s+/i;
+      let tags = d.title.split(splitTag)[0].split('] ')
+      let body = splitTag.test(d.title) ? d.title.split(splitTag)[1] + ' - ' : ''
+      let title = tags[tags.length - 1]
+      let city = tags[0].replace('[', '')
+      let labels = d.labels.reduce((l, c) => l + ' - ' + c.name, '')
+      return {
+        url: d.html_url,
+        body,
+        title,
+        labels: d.labels,
+        city,
+        description: body + city + labels
+      };
+    })
+    let jobsElement = document.getElementById("jobs");
+    const jobsHTML = jobs.slice(0, 4).reduce((html, job) => {
+      const { title, url, description } = job
+      return [
+      html,
+      '<a href="', url, '" class="card-item" target="_blank">',
+      '<p class="card-item-title">', title, '</p>',
+      '<p class="card-item-description">', description, '</p>',
+      '</a>',
+      ].join('');
+    }, '');
+    jobsElement.insertAdjacentHTML('beforeend', jobsHTML);
+  });
+})
+.catch(function (err) {
+  console.log('Fetch Error:', err);
+});
+
+fetch('https://api.github.com/repos/devfsa/meetups/issues/7/comments', { headers: { "Accept": "text" } })
+.then((response) => {
+  response.json().then((data) => {
+    const reactionsFetchHeader = { "Accept": "application/vnd.github.squirrel-girl-preview+json" }
+        // will wait all fetch from comments' reactions
+        Promise.all(
+          data.map(d => {
+            const keynote = {
+              url: d.url,
+              html_url: d.html_url,
+              user: d.user,
+              body: d.body,
+              title: d.body.split('\r\n')[0].replace(/\*\*/g, '')
+            };
+            // will transform array of data into array of promises that returns data
+            return fetch(`${keynote.url}/reactions`, { headers: reactionsFetchHeader })
+            .then(res => res.json())
+            .then(reactions => Object.assign({}, keynote, { reactions: reactions || [] }))
+            .catch(err => {
+              console.log(`Error fetching reactions of ${keynote.title} // ${keynote.url}`)
+              console.error(err);
+              return Object.assign({}, keynote, { reactions: [] });
+            })
+          })
+          ).then(keynotes => {
+          // after fetching all comments' reactions, sort it and then slice it
+          let notes = keynotes
+          .sort((k1, k2) => k2.reactions.length - k1.reactions.length)
+          .slice(0, 4)
+          let html = '';
+          let keynotesElement = document.getElementById("keynotes");
+          const keynotesHTML = notes.reduce((html, keynote, i) => {
+            const { user, html_url, body, title, reactions } = keynote
+            return [
+            html,
+            '<div class="card keynote-card">',
+            '<div class="keynote-image">',
+            '<img src="', user.avatar_url, '" class="keynote-photo" />',
+            '</div>',
+            '<div class="keynote-desc">',
+            '<a href="', html_url, '" class="keynote-title" target="_blank">', title, '</a>',
+            '<div class="keynote-info">',
+            '<p class="keynote-author">@', user.login, '</p>',
+            '<a href="', html_url, '" class="keynote-vote ', i === 0 ? 'voted' : '', '" target="_blank">',
+            '<i class="far fa-arrow-alt-circle-up"></i>',
+            '<span class="keynote-vote-number">', reactions.length, '</span>',
+            '</a>',
+            '</div>',
+            '</div>',
+            '</div>',
+            ].join('');
+          }, '');
+          keynotesElement.insertAdjacentHTML('beforeend', keynotesHTML);
+        });
+        });
+})
+.catch(function (err) {
+  console.log('Fetch Error:', err);
+});
